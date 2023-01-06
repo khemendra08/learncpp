0378a1f2814594884e927574e2596a36bc626985
khemendra08 <121980206+khemendra08@users.noreply.github.com>
2023-01-06 23:26:43 +0530

    created a code for addition

---
+++ addition.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"enter a number : \n";
+    cin>>a;
+    cout<<"enter another number : \n";
+    cin>>b;
+    c = a + b;
+    cout<<"Sum = "<<c<<"\n";
+    return 0;
+}
